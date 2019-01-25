a963019eb601f13a0cc330e8769bd930ab7ec3f0
Felipe Vieira <feliperenanvieira@gmail.com>
2019-01-25 12:15:29 -0200

    Tela de login pronta

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 import requests, json
 
 app = Flask(__name__)
+app.secret_key = b'_1@#12qdasd%#$%$U%DFsd'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if session.get('token'):
+        return redirect(url_for('teste'))
+    if request.method == 'GET':
+        return render_template('login.html')
+    res = json.loads((requests.post('http://localhost:5000/api/v1/users/login', data=json.dumps({
+        'email': request.form['email'],
+        'password': request.form['password']
+    }), headers={
+        'content_type': 'application/json'
+    })).content.decode('utf-8'))
+
+    if res.get('error'):
+        flash(u'Login e/ou senha incorretos', 'error')
+        return redirect(url_for('login'))
+
+    session['token'] = res['jwt_token']
+    return redirect(url_for('teste'))
 
 
-@app.route('/')
-def hello():
-    first_route = requests.get('http://localhost:5000/')
-    return first_route.content
+@app.route('/teste', methods=['GET'])
+def teste():
+    if not session.get('token'):
+        return redirect(url_for('login'))
+    return render_template('posts.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
