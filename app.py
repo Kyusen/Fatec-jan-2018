b2f553ecf06a92b2ab32cd4165892f0f3afc4ebe
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-01-26 11:36:06 -0200

    view adicionada para buscar post por id

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request, session, redirect, url_for, flash
-import requests, json
+import requests
+import json
+import datetime
 
 app = Flask(__name__)
 app.secret_key = b'_1@#12qdasd%#$%$U%DFsd'
+BASE_API_URL = "http://localhost:5000/api/v1"
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -27,16 +31,32 @@ def login():
     return redirect(url_for('teste'))
 
 
+@app.route('/post/<string:post_id>', methods=['GET'])
+def get_post_by_id(post_id):
+    if not session.get('token'):
+        return redirect(url_for('login'))
+    post = json.loads(
+        requests.get(BASE_API_URL + '/blogposts/' + post_id, headers={
+            'api-token': session.get('token')
+        }).content.decode('utf-8')
+    )
+    post['created_at'] = post.get('created_at').split('T')[0]
+
+    return render_template('post.html', post=post)
+
+
 @app.route('/teste', methods=['GET'])
 def teste():
     if not session.get('token'):
         return redirect(url_for('login'))
     return render_template('posts.html')
 
+
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
