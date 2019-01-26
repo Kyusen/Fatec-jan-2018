6e823332b7f49fa3dfa2f43deda5613700829c4a
Felipe Vieira <feliperenanvieira@gmail.com>
2019-01-26 10:38:43 -0200

    Serviço de requisição à API para cadastrar o post pronto

--- app.py
+++ app.py
@@ -38,11 +38,22 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-@app.route('/cadastrar-post')
+@app.route('/cadastrar-post', methods=['GET', 'POST'])
 def cadastrarPost():
     if not session.get('token'):
         return redirect(url_for('login'))
-    return render_template('cadastrar-post.html')
+    if request.method == 'GET':
+        return render_template('cadastrar-post.html')
+    requests.post('http://localhost:5000/api/v1/blogposts/', data=json.dumps({
+        'title': request.form['title'],
+        'contents': request.form['contents']
+    }), headers={
+        'content_type': 'application/json',
+        'api-token': session['token']
+    })
+
+    flash(u'Post cadastrado com sucesso', 'message')
+    return redirect(url_for('cadastrarPost'))
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
